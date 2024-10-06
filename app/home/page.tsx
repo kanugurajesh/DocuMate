258987525badad696350d692dc04692d848d5fc8
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-06 13:58:22 +0530

    Adding the background snippets

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -14,9 +14,6 @@ export default function Home() {
 
   return (
     <div>
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
-      </div>
       <div className="h-screen flex flex-col items-center justify-center ">
         <RiveComponent
           className="h-48 w-48 cursor-pointer hover:scale-105 transition-all ease-in-out duration-400"
--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
+      </div>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
