7c5a63055ae51bd92b16fca666eb0bfc65030130
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-06 14:31:27 +0530

    Adding data files

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,6 +2,9 @@
 
 import { useRive } from "@rive-app/react-canvas";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import Data from "@/data/data";
 
 export default function Home() {
   const router = useRouter();
@@ -13,7 +16,17 @@ export default function Home() {
   });
 
   return (
-    <div>
+    <div className="relative">
+      <div className="absolute top-5 right-5">
+        <div className="flex gap-4">
+          <Link href={Data.socials.linkedin} target="__blank">
+            <Image src="/linkedin.png" alt="linkedin" width={25} height={25} />
+          </Link>
+          <Link href={Data.socials.github} target="__blank">
+            <Image src="/github.png" alt="linkedin" width={25} height={25} />
+          </Link>
+        </div>
+      </div>
       <div className="h-screen flex flex-col items-center justify-center ">
         <RiveComponent
           className="h-48 w-48 cursor-pointer hover:scale-105 transition-all ease-in-out duration-400"
---
+++ data/data.ts
@@ -0,0 +1,8 @@
+const Data = {
+  socials: {
+    linkedin: "https://www.linkedin.com/in/rajesh-kanugu-aba8a3254/",
+    github: "https://github.com/kanugurajesh",
+  },
+};
+
+export default Data;
