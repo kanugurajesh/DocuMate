dffce419a5ade024ad224b46a50c73985df15401
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-06 12:34:31 +0530

    Adding home page icon

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { useRive } from "@rive-app/react-canvas";
+
 export default function Home() {
+  const { RiveComponent } = useRive({
+    src: "/rive/man.riv",
+    stateMachines: "State Machine 1",
+    autoplay: true,
+  });
+
   return (
-    <div>
-      
+    <div className="h-screen flex flex-col items-center justify-center">
+      <RiveComponent className="h-48 w-48" />
     </div>
   );
 }
